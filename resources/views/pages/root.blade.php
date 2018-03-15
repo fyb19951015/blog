f7a09a323de8fe0301981f7a439ebd9746ca7e3c
blogs <934472858@qq.com>
2018-03-15 16:31:25 +0800

    login and register update

--- resources/views/pages/root.blade.php
+++ resources/views/pages/root.blade.php
@@ -2,5 +2,6 @@
 @section('title', '哲♂学♀')
 
 @section('content')
-    <h1>这里是首页</h1>
+    <h1 class="text-center" >欢迎来到首页</h1>
+    <body  background="https://i.pximg.net/img-original/img/2018/03/13/00/04/49/67705998_p0.png">
 @stop
